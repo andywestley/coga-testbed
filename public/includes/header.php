c2ab8d967d3ff0ff990b3ce7a591bf6b60ea804a
andywestley <andy@andrewwestley.co.uk>
2026-07-04 11:09:35 +0100

    Update Google Tag Manager ID

--- public/includes/header.php
+++ public/includes/header.php
@@ -10,7 +10,7 @@ $base_path = '/';
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W8HCXHG5');</script>
+    })(window,document,'script','dataLayer','GTM-NFSRSZ7C');</script>
     <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,7 +26,7 @@ $base_path = '/';
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8HCXHG5"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFSRSZ7C"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <header>
